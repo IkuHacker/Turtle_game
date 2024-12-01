c9994b50857c64d4a4ea1f737b4ace7602187317
Mic <micfiverr@gmail.com>
2024-12-01 20:28:08 +0100

    Revert "iubiuzbi"
    This reverts commit 068c23f69d64d2618bd1d04bf9e85514d880b41b.

--- Scene/Walk.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-func _ready():
-	$Animations.play("Walk")
-
-func _process(delta):
-	var Direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = Direction * 200
-	move_and_slide()
