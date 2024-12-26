bbb09aa23a4a0fc43c118ca4cb1122fa3ca295d2
Mic <micfiverr@gmail.com>
2024-12-26 13:51:19 +0100

    Fixe juste la caméra et je m'occupe du reste ^^

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0  # Vitesse maximale
-@export var ACCELERATION = 1000.0  # Taux d'accélération
-@export var DECELERATION = 800.0  # Taux de décélération
+@export var Speed = 200.0  # [Float] Vitesse maximale
+@export var Acceleration = 1000.0  # [Float] Taux d'accélération
+@export var Deceleration = 1200.0  # [Float] Taux de décélération
+var Velocity = Vector2()
+var MousePos = null
 
-
-func _enter_tree() -> void:
+# [Func] Gère le multijoueur
+func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 	
 	 # Activer ou désactiver la caméra selon si c'est le joueur local ou non
@@ -18,28 +20,7 @@ func _enter_tree() -> void:
 		# Désactiver la caméra pour les autres joueurs
 		$Camera2D.enabled = false
 		$CollisionShape2D.disabled = true
-		
 
-
-func _physics_process(delta: float) -> void:
-	$AnimationPlayer.play()
-	if is_multiplayer_authority():
-
-		var input_dir = Vector2(
-			Input.get_axis("move_left", "move_right"),
-			Input.get_axis("move_up", "move_down")
-		).normalized()
-
-		# Si une direction est enfoncée, on accélère vers la vitesse cible dans cette direction.
-		if input_dir != Vector2.ZERO:
-			velocity = velocity.move_toward(input_dir * SPEED, ACCELERATION * delta)
-			$AnimationPlayer.current_animation = "walk"
-			$Sprite2D.flip_h = input_dir.x > 0  # Inverse le sprite si on va vers la gauche
-
-		else:
-			# Si aucune direction n'est enfoncée, applique une décélération pour arrêter progressivement.
-			velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
-			$AnimationPlayer.current_animation = "idle"
-
-		# Appliquer la vitesse pour déplacer le personnage.
-		move_and_slide()
+func _physics_process(delta):
+	position = get_global_mouse_position()
+	print(position == get_global_mouse_position())
